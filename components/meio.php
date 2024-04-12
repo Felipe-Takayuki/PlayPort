635dde84f09ec5526a8a00c3d2ec08cd8bd6b178
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-04-12 08:42:57 -0300

    adicionando o meio do site

---
+++ components/meio.php
@@ -0,0 +1,18 @@
+
+<main>
+    <article class="meio">
+        <h1 class="textLogo">PlayPort</h1>
+        <p class="locadoraDe">Locadora da jogos</p>
+        <div class="destaque">
+            <p>Em destaque: </p>
+            <div class="jogos">
+                <div class="card">jogo bom</div>
+                <div class="card">jogo bom</div>
+                <div class="card">jogo bom</div>
+            </div>
+        </div>
+    </article>
+</main>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php 
 require_once 'components/topo.php';
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+    switch($p) {
+        case 'home': 
+            require_once 'components/meio.php';
+            break;
+    }
+}else {
+    require_once 'components/meio.php';
+}
+
 ?>
 </body>
 </html>
