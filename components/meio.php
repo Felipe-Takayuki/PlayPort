e05c7004fbaaf43e6dd0cee5ee9f9d28420b089d
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-04-14 13:46:40 -0300

    parte sobre a empresa na home adicionada

--- components/meio.php
+++ components/meio.php
@@ -10,6 +10,22 @@
                 <div class="card">jogo bom</div>
                 <div class="card">jogo bom</div>
             </div>
+            <div class="empresa">
+                <div class="imagem">
+
+                </div>
+                <div class="resumo-empresa">
+                    <div class="text">
+                        <h1>Empresa:</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum massa a enim venenatis luctus. Mauris pretium lorem sit amet elit dapibus maximus et in enim.
+                        </p> 
+                    </div>
+                    <a href="index.php?page=empresa">
+                        <div class="button">Saiba Mais</div>
+                    </a>
+                </div>
+            </div>
         </div>
     </article>
 </main>
